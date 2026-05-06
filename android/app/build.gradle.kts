c6b720a1541f0e842b860b7e6cd944f2464d6428
Amr Saleh <amrsalehma@gmail.com>
2026-05-06 03:37:48 +0300

    Configure Android project for spotify_sdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,10 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders += mapOf(
+            "redirectSchemeName" to "com.amoorsaleh.musix",
+            "redirectHostName" to "callback"
+        )
     }
 
     buildTypes {
